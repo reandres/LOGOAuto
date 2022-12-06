f15f08ca79359239f2c33a2705487f73e71dee01
Andrey <reandres20000@gmail.com>
2022-12-06 22:45:55 +0200

    add slider

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.reviews__inner').slick({
+        infinite: true,
+        speed: 1200,
+        arrows: false,
+        dots: true,
+        variableWidth: true,
+        slidesToShow: 1
+    });
+});
+  
