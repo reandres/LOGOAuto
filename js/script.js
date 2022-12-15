ed65e3f37d613896a014bff9f0bb4faa7ed4f658
Andrey <reandres20000@gmail.com>
2022-12-15 10:47:42 +0200

    add max-width 991 and hamburger menu

--- js/script.js
+++ js/script.js
@@ -4,7 +4,28 @@ $(document).ready(function(){
         speed: 1200,
         arrows: false,
         dots: true,
-        slidesToShow: 1    
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
     });
 });
+
+
+
+
+
+// hamburger menu
+window.addEventListener('DOMContentLoaded', function() {
+    const menu = document.querySelector('.header__wrapper_mobile');
+    const hamburger = document.querySelector('.hamburger_menu');
+    const logo = document.querySelector('.header');
+    
+    hamburger.addEventListener('click', function() {
+        hamburger.classList.toggle('hamburger_menu_active');
+        menu.classList.toggle('header__wrapper_mobile_active');
+        logo.classList.toggle('header_active');
+    });
+}); 
+  
+
   
