0de1cfcb575fdab1e753a2671f7aaf388924ca05
Andrey <reandres20000@gmail.com>
2022-12-26 09:55:23 +0200

    test mobile slider

--- js/script.js
+++ js/script.js
@@ -1,18 +1,68 @@
 $(document).ready(function(){
     $('.reviews__inner').slick({
         infinite: true,
-        speed: 1200,
+        speed: 1000,
         arrows: false,
         dots: true,
         slidesToShow: 1,
         autoplay: true,
-        autoplaySpeed: 2000
+        autoplaySpeed: 3000
     });
 });
 
+// mobile slider competitions
+$('.competitions__wrapper').slick({
+    slidesToShow: 1.1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
 
+  $(function(){
+    resizeSliderContent = false;
+    function resize_slider_content(){
+      if ($(window).width() <= 644 && resizeSliderContent == true) { 
+        $('.competitions__wrapper',).slick({
+            arrows: false
+        });
+        resizeSliderContent = false;
+      } 
+      else if ($(window).width() > 644 && resizeSliderContent == false){
+        $('.competitions__wrapper').slick('unslick');
+        resizeSliderContent = true;
+      };
+    }
+    resize_slider_content();
+    $(window).on('resize', function(){
+        resize_slider_content();
+    });
+});
 
+// mobile slider latest
+$('.latest__wrapper').slick({
+    slidesToShow: 1.1,
+    arrows: false
+  });
 
+  $(function(){
+    resizeSliderContent = false;
+    function resize_slider_content(){
+      if ($(window).width() <= 644 && resizeSliderContent == true) { 
+        $('.latest__wrapper',).slick({
+            arrows: false
+        });
+        resizeSliderContent = false;
+      } 
+      else if ($(window).width() > 644 && resizeSliderContent == false){
+        $('.latest__wrapper').slick('unslick');
+        resizeSliderContent = true;
+      };
+    }
+    resize_slider_content();
+    $(window).on('resize', function(){
+        resize_slider_content();
+    });
+});
 
 // hamburger menu
 window.addEventListener('DOMContentLoaded', function() {
